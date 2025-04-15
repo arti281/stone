2a50657e461c2c4ceb8e531c36de97301ee17ae3
Ramnarayan <rnm312000@gmail.com>
2025-04-16 00:43:31 +0530

    init

--- app/Models/Admin/Users/User.php
+++ app/Models/Admin/Users/User.php
@@ -7,11 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Model
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable;
     
     protected $table = 'users';
     
