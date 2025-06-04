fafededc0a3fa8ef9562a0dbddec81d713c21d96
Ramnarayan <rnm312000@gmail.com>
2025-06-04 22:56:07 +0530

    Fixed route

--- routes/web.php
+++ routes/web.php
@@ -102,6 +102,7 @@ Route::name('catalog.')->group(function () {
     Route::get('return-replacement-policy', [InformationController::class, 'privacyPolicy'])->name('privacyPolicy');
     Route::get('/aboutus', [InformationController::class, 'about'])->name('aboutus');
     Route::get('/contactus', [ContactController::class, 'show'])->name('contactus');
+    Route::post('/contactus', [ContactController::class, 'submit']);
     
     
 
